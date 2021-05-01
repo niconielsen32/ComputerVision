ea4d1a9f528975f2c3a78a87394106a076ad16a6
Nicolai Høirup Nielsen <55984156+niconielsen32@users.noreply.github.com>
2021-05-01 13:16:05 +0200

    Add files via upload

---
+++ VSCodeOpenCV/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+int main(int argc, char** argv )
+{
+
+    Mat image;
+    image = imread("C:/Users/kemik/OneDrive/Skrivebord/Images/lena.png");
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
