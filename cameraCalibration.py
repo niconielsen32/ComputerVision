f565df758bdad44a1228de9dd0a73977deebb99b
Nicolai Høirup Nielsen <55984156+niconielsen32@users.noreply.github.com>
2021-10-21 22:39:54 +0200

    Update cameraCalibration.py

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -19,6 +19,9 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)
 
+size_of_chessboard_squares_mm = 20
+objp = objp * size_of_chessboard_squares_mm
+
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
