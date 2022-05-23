b31a2829061eff4875497e5bff53db2a2b414e9e
Yatharth Shah <34169423+yatharth9@users.noreply.github.com>
2022-05-23 14:16:34 +0530

    Removed AttributeError by updating changed Attribute Name
    https://github.com/google/mediapipe/issues/2448 In Mediapipe, they have renamed FACE_CONNECTIONS to FACE_CONTOURS. This PR replaces the old Attribute Name with the new Attribute Name.

--- headPoseEstimation.py
+++ headPoseEstimation.py
@@ -123,7 +123,7 @@ while cap.isOpened():
         mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
                     landmark_drawing_spec=drawing_spec,
                     connection_drawing_spec=drawing_spec)
 
